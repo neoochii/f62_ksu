5131624131b1661a93361ae1648d0b14fad2cb39
root <dipak.wwe1@gmail.com>
2025-06-04 12:09:19 +0530

    spelling error
    Signed-off-by: root <dipak.wwe1@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ RDIR="$(pwd)"
 
 export PLATFORM_VERSION=13
 export ARCH=arm64
-build_ksu(){Add commentMore actions
+build(){Add commentMore actions
     export KSU_STATUS="ksu"
     make ${ARGS} neoochii_defconfig common.config ksu.config
     make ${ARGS} menuconfig
