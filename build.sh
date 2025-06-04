49d7049d6986e8864ed3cbef6c8d9d27b1bf389e
root <dipak.wwe1@gmail.com>
2025-06-04 11:53:13 +0530

    added build.sh
    Signed-off-by: root <dipak.wwe1@gmail.com>

---
+++ build.sh
@@ -0,0 +1,14 @@
+make clean && make mrproper
+@@ -1,5 +1,6 @@
+#!/bin/bash
+RDIR="$(pwd)"
+
+export PLATFORM_VERSION=13
+export ARCH=arm64
+build_ksu(){Add commentMore actions
+    export KSU_STATUS="ksu"
+    make ${ARGS} neoochii_defconfig common.config ksu.config
+    make ${ARGS} menuconfig
+    make ${ARGS}
+}
+build
--- build_kernel.sh
+++
@@ -1,7 +0,0 @@
-make clean && make mrproper
-
-export PLATFORM_VERSION=13
-export ARCH=arm64
-
-make ARCH=arm64 physwizz_defconfig
-make ARCH=arm64
