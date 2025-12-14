f836fd21534d3324511463f21395a555e2227cfd
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-12-14 10:14:04 +0800

    [BACKPORT] fs: implement susfs v2.0.0
    * sus_mount and others are complied with the upstream susfs kernel branch v5.10
    * susfs try_umount is restored
    * auto features are deprecated so you have to hide your mounts manually
    
    This susfs backport commit requires a set of backports to comply with the upstream susfs:
    d406904bb175cb9791b5aed4404df3268859d91b fs: Convert namespace IDAs to new API
    9ba899398576d329288b023566d3a731470ad13a fs: Convert unnamed_dev_ida to new API
    e784224542fab4b3888d9103a1f678015221a7d1 ida: Add new API
    ed8fd10d97a50ec424ae38a40351b5de060f9838 ida: Lock the IDA in ida_destroy
    4ba6dccf73a620e565803e5995d5748ecfbb56a2 lib/idr.c: remove simple_ida_loc
    0a8cbf3 proc: less memory for /proc/*/map_files readdir
    37ae244 mm: smaps: split PSS into components
    49a5115 fs/task_mmu: add pkeys header
    6f94042 mm/pkeys: Add an empty arch_pkeys_enabled()
    bbd5aec mm/pkeys, powerpc, x86: Provide an empty vma_pkey() in linux/pkeys.h
    849ca8c mm: /proc/pid/smaps_rollup: convert to single value seq_file
    6071a48 mm: /proc/pid/smaps: factor out common stats printing
    03fd2fb mm: /proc/pid/smaps: factor out mem stats gathering
    95f8be4 mm: /proc/pid/*maps remove is_pid and related wrappers
    27956d2 procfs: add seq_put_hex_ll to speed up /proc/pid/maps
    7f2847d proc: add seq_put_decimal_ull_width to speed up /proc/pid/smaps
    
    For KernelSU, Use my RKSU fork which it includes susfs try_umount:
    https://github.com/sidex15/KernelSU/tree/susfs-rksu-master
    
    Or if you want to patch it within RKSU, use this commit:
    https://github.com/sidex15/KernelSU/commit/8be13a387a7b32f6c70b9077a97c30840254f630

--- fs/open.c
+++ fs/open.c
@@ -31,6 +31,9 @@
 #include <linux/ima.h>
 #include <linux/dnotify.h>
 #include <linux/compat.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif
 
 #include "internal.h"
 
@@ -357,9 +360,17 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	}
 	return error;
 }
-// KernelSU hook
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS)
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
+#endif
+#ifdef CONFIG_KSU_SUSFS
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-    int *flags);
+			int *flags);
+#endif
 
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
@@ -367,17 +378,8 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int
  * switching the fsuid/fsgid around to the real ones.
  */
 
-#ifdef CONFIG_KSU
-__attribute__((hot))
-extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
-int *mode, int *flags);
-#endif
-
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
-#ifdef CONFIG_KSU
-ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-#endif
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
@@ -385,6 +387,20 @@ ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_SUSFS)
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted())) {
+		goto orig_flow;
+	}
+
+	if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
+		ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+	}
+
+orig_flow:
+#endif
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
 
