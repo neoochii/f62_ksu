6259480830cbbeff06e6eff141f92f86a98e4e6f
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2026-04-22 16:38:45 -0400

    Revert "fs: susfs: Make sus_mounts invisible only for apps that have sus_mounts umounted"
    This reverts commit 837298a8037ce3686481dfb40325a0c2317250fd.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -112,7 +112,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID)&&(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
@@ -155,7 +155,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID)&&(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 	
@@ -226,7 +226,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID)&&(susfs_hide_sus_mnts_for_all_procs || !susfs_is_current_ksu_domain()))
 		return 0;
 #endif
 
