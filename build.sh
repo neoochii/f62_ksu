302e735163cb29fe3d03921f5310becca46cc6b1
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2026-04-23 03:46:18 -0400

    build.sh: Bump kernel version to v6.0 and remove KernelSU-Next from the script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 RDIR="$(pwd)"
 
@@ -6,9 +5,9 @@ export PLATFORM_VERSION=13
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=t
 MODEL="GalaxyF62"
-BUILD_KERNEL_VERSION="V5.8"
+BUILD_KERNEL_VERSION="V6.0"
 
-# Check for -d flag
+# Check for debug flag
 DEBUG_BUILD=false
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
@@ -22,72 +21,79 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-#init ksu next
-curl -LSs "https://raw.githubusercontent.com/GoRhanHee/KernelSU-Next/next-susfs/kernel/setup.sh" | bash - 
-
-# Install requirements
-if [ ! -f ".requirements" ]; then
-    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
-        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
-        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
-        make repo cpio kmod openssl libelf-dev pahole libssl-dev --fix-missing && wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb && sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb && touch .requirements
-fi
-
-#build dir
+# build dir
 if [ ! -d "${RDIR}/build" ]; then
     mkdir -p "${RDIR}/build"
 else
-    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
-fi 
+    rm -rf "${RDIR}/build"
+    mkdir -p "${RDIR}/build"
+fi
 
-# main variables
+# build args
 export ARGS="
 -j$(nproc)
 ARCH=arm64
 "
-# create flashable zip from buildzip folder
 
+# create flashable zip
 build_zip() {
- 
- 
-  echo "[+] Creating flashable zip..."
+    echo "[+] Creating flashable zip..."
 
- ZIP_NAME="KernelSU-Next-${MODEL}-${BUILD_KERNEL_VERSION}.zip"
- rm -f  ${RDIR}/buildzip/Image
- cp "${RDIR}/arch/arm64/boot/Image"  "${RDIR}/buildzip/"
- cd "${RDIR}/buildzip"
- zip -r9 "../build/${ZIP_NAME}" ./*
- echo "[✓] Flashable zip created at build/${ZIP_NAME}"
-  cd "${RDIR}"
+    ZIP_NAME="${MODEL}-${BUILD_KERNEL_VERSION}.zip"
+
+    rm -f "${RDIR}/buildzip/Image"
+    cp "${RDIR}/arch/arm64/boot/Image" "${RDIR}/buildzip/"
+
+    cd "${RDIR}/buildzip" || exit 1
+    zip -r9 "../build/${ZIP_NAME}" ./*
+
+    echo "[✓] Flashable zip created at build/${ZIP_NAME}"
+
+    cd "${RDIR}" || exit 1
 }
 
+# build boot image
+build_boot() {
+    rm -f \
+      "${RDIR}/AIK-Linux/split_img/boot.img-kernel" \
+      "${RDIR}/AIK-Linux/boot.img" \
+      "${RDIR}/build/boot.img"
 
-#build boot.img
-build_boot() {    
-    rm -f ${RDIR}/AIK-Linux/split_img/boot.img-kernel ${RDIR}/AIK-Linux/boot.img ${RDIR}/build/boot.img
-    cp "${RDIR}/arch/arm64/boot/Image" ${RDIR}/AIK-Linux/split_img/boot.img-kernel
-    echo $BOARD > ${RDIR}/AIK-Linux/split_img/boot.img-board
-    mkdir -p ${RDIR}/AIK-Linux/ramdisk
-    cd ${RDIR}/AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img ${RDIR}/build/boot.img
+    cp "${RDIR}/arch/arm64/boot/Image" \
+       "${RDIR}/AIK-Linux/split_img/boot.img-kernel"
+
+    echo "$BOARD" > "${RDIR}/AIK-Linux/split_img/boot.img-board"
+
+    mkdir -p "${RDIR}/AIK-Linux/ramdisk"
+
+    cd "${RDIR}/AIK-Linux" || exit 1
+    ./repackimg.sh --nosudo
+    mv image-new.img "${RDIR}/build/boot.img"
+
+    cd "${RDIR}" || exit 1
 }
 
-#build odin flashable tar
-build_tar(){
-    cd ${RDIR}/build
-    tar -cvf "KernelSU-Next-${MODEL}-${BUILD_KERNEL_VERSION}-stock-One-UI.tar" boot.img 
-    echo -e "\n[i] Build Finished..!\n" && cd ${RDIR}
+# build odin tar
+build_tar() {
+    cd "${RDIR}/build" || exit 1
+
+    tar -cvf "${MODEL}-${BUILD_KERNEL_VERSION}-stock-One-UI.tar" boot.img
+
+    echo -e "\n[i] Build Finished..!\n"
+
+    cd "${RDIR}" || exit 1
 }
 
-# building function
+# kernel build
 build() {
     if [ "$DEBUG_BUILD" = true ]; then
-        echo "[!] Building with debug.config (permissive)"
+        echo "[!] Building with all configs + debug.config"
         make ${ARGS} neoochii_defconfig common.config ksu.config debug.config
     else
-        echo "[!] Building without debug.config"
+        echo "[!] Building with all configs"
         make ${ARGS} neoochii_defconfig common.config ksu.config
     fi
-    
+
     make ${ARGS} || exit 1
 }
 
